cafa678e86a157bef3b9a739f8cb38bb24a4a775
ambs <ambs@cpan.org>
2013-07-20 20:18:07 +0100

    Warnings and validation

--- lib/Termomatic.pm
+++ lib/Termomatic.pm
@@ -1,13 +1,21 @@
 package Termomatic;
+
 use Template;
+
 use Dancer2 ':syntax';
+use Dancer2::Plugin::Deferred;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'login' => {
+                         failed_login => 1,
                          title => 'Sign-in',
                         };
+};
+
+get '/register' => sub {
+    
 };
 
 true;
